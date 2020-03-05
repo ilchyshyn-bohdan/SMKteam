b473a093319c5479fbc011eb4d4b7eb50422fdd7
ilchyshyn-bohdan <43094332+ilchyshyn-bohdan@users.noreply.github.com>
2020-03-05 17:04:39 +0200

    Add serializers.py, serializers.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
-from .views import GoogleLoginView
+from .views import GoogleLoginView, UserViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls'), name='auth'),
     path('auth/registration/', include('rest_auth.registration.urls')),
     path('auth/google/', GoogleLoginView.as_view(), name='google_login'),
+
+    path('', include(router.urls))
 ]
