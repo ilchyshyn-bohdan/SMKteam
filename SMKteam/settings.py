9c30640ace00123c1c25a4a40b48d87d3320d0c9
ilchyshyn-bohdan <43094332+ilchyshyn-bohdan@users.noreply.github.com>
2020-03-05 12:16:13 +0200

    Create a new app for authentication

--- SMKteam/settings.py
+++ SMKteam/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'authentication',
 ]
 
 ROOT_URLCONF = 'SMKteam.urls'
